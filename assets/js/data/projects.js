9f45825d4fe40c5fddfa4da10a298d94053a8948
n1akai <rakansubs@gmail.com>
2025-02-27 22:10:00 +0000

    Add project details

---
+++ assets/js/data/projects.js
@@ -0,0 +1,77 @@
+export const projects = [
+  {
+    title: "ABOULHODA - Portfolio",
+    shortTitle: "Aboulhoda",
+    description:
+      "This is my first personal portfolio, designed with a simple and clean style, which align with what my preference, I built it without any frameworks to showcase my ability to create a functional and visually appealing website using only the core technologies.",
+    date: "Personal Portfolio | Feb 2025",
+    imgs: [
+      "/assets/images/projects/aboulhoda/Home.webp",
+      "/assets/images/projects/aboulhoda/About.webp",
+      "/assets/images/projects/aboulhoda/Projects.webp",
+      "/assets/images/projects/aboulhoda/Contact.webp",
+      "/assets/images/projects/aboulhoda/Project-Details.webp",
+    ],
+    tags: ["HTML", "CSS", "JavaScript", "2025"],
+    moreDetailsTarget: "aboulhoda-details",
+    technologies: [
+      { icon: "devicon-html5-plain-wordmark colored", text: "HTML" },
+      { icon: "devicon-css3-plain-wordmark colored", text: "CSS" },
+      { icon: "devicon-javascript-plain colored", text: "JavaScript" },
+      { icon: "fa-solid fa-mobile", text: "Responsive" },
+    ],
+    links: [
+      { icon: "fa-solid fa-globe", text: "Demo", link: "#" },
+      { icon: "fa-brands fa-github", text: "Github", link: "#" },
+    ],
+  },
+  {
+    title: "Doros Online - CMS",
+    shortTitle: "Doros Online",
+    description:
+      "The goal of this project was to create a simple and clean WordPress theme within a short timeframe. I aimed to make it easily understandable for those unfamiliar with WordPress theme development. To achieve this, I structured it similarly to Laravel and built it using the Themosis Framework.",
+    date: "Doros Online | Feb 2025",
+    imgs: ["/assets/images/projects/Doros Online.webp"],
+    tags: [
+      "HTML",
+      "CSS",
+      "Vue.js",
+      "Bootstrap",
+      "Wordpress",
+      "Themosis",
+      "2024",
+    ],
+    moreDetailsTarget: "doros-online-details",
+    technologies: [
+      { icon: "devicon-html5-plain-wordmark colored", text: "HTML" },
+      { icon: "devicon-css3-plain-wordmark colored", text: "CSS" },
+      { icon: "devicon-vuejs-plain colored", text: "Vue.js" },
+      { icon: "devicon-bootstrap-plain-wordmark colored", text: "Bootstrap" },
+      { icon: "devicon-wordpress-plain-wordmark colored", text: "Wordpress" },
+      { icon: "fa-solid fa-mobile", text: "Responsive" },
+    ],
+    links: [
+      { icon: "fa-solid fa-globe", text: "Demo", link: "#" },
+      { icon: "fa-brands fa-github", text: "Github", link: "#" },
+    ],
+  },
+  // New projects
+  {
+    title: "Games Store - Admin Dashboard",
+    shortTitle: "Games Store",
+    date: "Games Store | Feb 2025",
+    description:
+      "This project is designed to manage the data of a mobile application called Games Store. I built it using Vue.js, the first JavaScript framework I learned back in 2023. For state management, I used Pinia, and I incorporated Vuetify components to maintain visual consistency with the mobile app",
+    imgs: ["/assets/images/projects/Games Store.webp"],
+    tags: ["Vue.js", "Pinia", "Vuetify", "Firebase", "HTML", "CSS", "2023"],
+  },
+  {
+    title: "Games Store - Native Android Application",
+    shortTitle: "Games Store",
+    description:
+      "This mobile application was built using the Android SDK, allowing players to purchase their favorite games effortlessly. It features a clean and simple UI, ensuring a seamless user experience.",
+    date: "Game Store | Feb 2025",
+    imgs: ["/assets/images/projects/Games Store - Mobile.webp"],
+    tags: ["Android", "Java", "XML", "Firebase", "2023"],
+  },
+];
---
+++ assets/js/src/projects.js
@@ -0,0 +1,174 @@
+import { select, selectAll } from "../utils.js";
+import { projects as data } from "../data/projects.js";
+
+export function projects() {
+  select("#projects .cards-wrapper").innerHTML = renderProjects(data);
+
+  const targets = selectAll("[more-details-target]");
+
+  targets.forEach((element) => {
+    element.addEventListener("click", () => {
+      const targetId = element.getAttribute("more-details-target");
+      const project = data.find(
+        (element) => element.moreDetailsTarget === targetId
+      );
+      select(".project-details .container").innerHTML = renderDetails(project);
+      /**
+       * Show details
+       */
+      const elementTarget = select(".project-details");
+      elementTarget.classList.add("active");
+      /**
+       * Close Button
+       */
+      select(".project-details .content .close").addEventListener(
+        "click",
+        () => {
+          elementTarget.classList.remove("active");
+        }
+      );
+      /**
+       * Next & Prev Buttons
+       */
+      const slider = select("#slider");
+      select("#slider-prev").addEventListener("click", () => {
+        const sliderWidth = slider.offsetWidth;
+        slider.scrollLeft -= sliderWidth;
+      });
+
+      select("#slider-next").addEventListener("click", () => {
+        const sliderWidth = slider.offsetWidth;
+        slider.scrollLeft += sliderWidth;
+      });
+      /**
+       * Expand & Minimize Buttons
+       */
+      select("#expand").addEventListener("click", () => {
+        // this is important for fix a problem, when expand is activated
+        slider.scrollLeft = 0;
+        // Change close button to white
+        select(".project-details .container .content .close").style.color =
+          "white";
+        select(
+          ".project-details .container .content .details .images-wrapper"
+        ).classList.add("expand");
+        select("#expand").classList.toggle("active");
+        select("#minimize").classList.toggle("active");
+      });
+      select("#minimize").addEventListener("click", () => {
+        slider.scrollLeft = 0;
+        select(".project-details .container .content .close").style.color = "";
+        select(
+          ".project-details .container .content .details .images-wrapper"
+        ).classList.remove("expand");
+        select("#expand").classList.toggle("active");
+        select("#minimize").classList.toggle("active");
+      });
+
+      select("#slider-next").addEventListener("click", () => {
+        const sliderWidth = slider.offsetWidth;
+        slider.scrollLeft += sliderWidth;
+      });
+    });
+  });
+}
+
+function renderProjects(projectsArray) {
+  return projectsArray
+    .map(
+      (project) => `
+    <div class="card">
+      <div class="card-img${project.moreDetailsTarget ? " more-details" : ""}" 
+           ${
+             project.moreDetailsTarget
+               ? `more-details-target="${project.moreDetailsTarget}"`
+               : ""
+           }>
+        <img src="${project.imgs[0]}" />
+      </div>
+      <div class="card-body">
+        <h4 class="card-title">${project.title}</h4>
+        <div class="card-description line line-clamp-2">
+          ${project.description}
+        </div>
+        <div class="badges-wrapper">
+          ${project.tags
+            .map((tag) => `<div class="badge">${tag}</div>`)
+            .join("")}
+        </div>
+      </div>
+    </div>
+  `
+    )
+    .join("");
+}
+
+function renderDetails(detail) {
+  return `<div class="content">
+      <div class="close">
+        <i class="fa-solid fa-xmark fa-xl"></i>
+      </div>
+      <div class="details">
+        <div class="images-wrapper">
+          <ul id="slider" class="hide-scroll">
+            ${detail.imgs
+              .map(
+                (img) => `
+              <li>
+                <img src="${img}" />
+              </li>
+            `
+              )
+              .join("")}
+          </ul>
+          <div id="expand">
+            <i class="fa-solid fa-expand"></i>
+          </div>
+          <div id="minimize" class="active">
+            <i class="fa-solid fa-minimize"></i>
+          </div>
+          <div id="slider-prev">
+            <i class="fa-solid fa-chevron-left"></i>
+          </div>
+          <div id="slider-next">
+            <i class="fa-solid fa-chevron-right"></i>
+          </div>
+        </div>
+        <div class="info">
+          <h3 class="title">${detail.shortTitle}</h3>
+          <p class="date">${detail.date}</p>
+          <div class="my-2"></div>
+          <p class="description">${detail.description}</p>
+          <div class="my-2"></div>
+          <h4>Technologies and Features</h4>
+          <div class="my-2"></div>
+          <div class="technologies">
+            ${detail.technologies
+              .map(
+                (tech) => `
+              <div class="tech">
+                <i class="${tech.icon}"></i> ${tech.text}
+              </div>
+            `
+              )
+              .join("")}
+          </div>
+          <div class="my-2"></div>
+          <h4>Links</h4>
+          <div class="my-2"></div>
+          <div class="link">
+            ${detail.links
+              .map(
+                (link) => `
+              <a href="${link.link}">
+                <i class="${link.icon}"></i> ${link.text}
+              </a>
+            `
+              )
+              .join("")}
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
